5d1c7879a53d0ebcb8b89679225f41e8f2cabf99
jrtaboraman <jr.taboraman@gmail.com>
2022-07-31 00:16:59 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+
+      <Navbar dark color="primary">  
+      <div className="container"> 
+      <NavbarBrand href="/">Latest React Course</NavbarBrand>
+
+      </div>
+      </Navbar> 
+  );
+}
+
+export default App;
