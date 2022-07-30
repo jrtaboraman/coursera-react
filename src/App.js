1d14167588920e550dd684a746dae1e03ceb59e9
jrtaboraman <jr.taboraman@gmail.com>
2022-07-31 02:30:35 +0800

    components part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
   return (
-
+      <>
+      <div> 
       <Navbar dark color="primary">  
       <div className="container"> 
       <NavbarBrand href="/">Latest React Course</NavbarBrand>
-
+      
       </div>
       </Navbar> 
+      <Menu />
+      </div>
+      
+      </>
   );
 }
 
